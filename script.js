a39bdbe554eec2dd86d62eb592e16eb2a6ae3ab2
SaraIsabella01 <saraisabelescobar4875@gmail.com>
2024-06-06 09:21:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    const totalSlides = slides.length;
+
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalSlides - 2;
+    } else {
+        currentIndex = index;
+    }
+
+    const offset = -currentIndex * 100;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+function moveSlide(step) {
+    showSlide(currentIndex + step);
+}
+
+setInterval(() => {
+    moveSlide(1);
+}, 3000);
